36cdfea149691f3d32b1c3c61fd56e23e6f4e840
kaviyanSL <kaviyan.lari.71@gmail.com>
2025-02-13 12:57:54 +0330

    change the singelton pattern to general pattern to use multiprocessing

--- src/multiprocess_service/MultiprocessPreprocessText.py
+++ src/multiprocess_service/MultiprocessPreprocessText.py
@@ -1,34 +1,37 @@
+import logging
 from src.services.LanguageDetectionService import LanguageDetectionService
-from src.services.ClusteringService import ClusteringService
-from src.services.NLPBasedModelsService import NLPBasedModelsService
-from src.services.TextPreProcessorService import TextPreProcessorService
 from src.multiprocess_service.Batching import Batching
 from src.database.RawCommentRepository import RawCommentRepository
 from multiprocessing import Pool
 import pandas as pd
 import os
 
+# Configure logging
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+
+# Top-level function for language detection
+def detect_language(comment):
+    language_detection_service = LanguageDetectionService()
+    return language_detection_service.detect_language(comment)
+
 class MultiprocessPreprocessText:
     def __init__(self):
-        self.LanguageDetectionService = LanguageDetectionService()
-        self.ClusteringService = ClusteringService()
-        self.NLPBasedModelsService = NLPBasedModelsService()
-        self.TextPreProcessorService = TextPreProcessorService()
         self.Batching = Batching()
-        self.RawCommentRepository = RawCommentRepository()
 
-    def detect_language(self, comment):
-        return self.LanguageDetectionService.detect_language(comment)
+    def update_language(self, comment_id, language):
+        RawCommentRepository().updating_language(comment_id, language)
 
     def multiprocess_language_detection(self):
         all_data, number_of_record_per_patch = self.Batching.Batchsize()
         batch_size = 0
         while batch_size < all_data:
+            logging.debug(f"Processing batch from {batch_size} to {batch_size + number_of_record_per_patch}")
             batchsized_data = self.Batching.call_batchsized_data(batch_size, batch_size + number_of_record_per_patch)
             batchsized_data = pd.DataFrame(batchsized_data)
             with Pool(processes=os.cpu_count() - 4) as p:
-                languages = p.map(self.detect_language, batchsized_data['comment'])
+                languages = p.map(detect_language, batchsized_data['comment'])
                 for idx, language in enumerate(languages):
-                    self.RawCommentRepository.updating_language(batchsized_data.iloc[idx]['id'], language)
+                    self.update_language(batchsized_data.iloc[idx]['id'], language)
+                    logging.debug(f"Updated language for comment ID {batchsized_data.iloc[idx]['id']} to {language}")
 
             batch_size += number_of_record_per_patch
