c3aa07434556eb422cc966ea9834cd8eed49dfe1
kaviyanSL <kaviyan.lari.71@gmail.com>
2025-02-25 21:00:16 +0330

    saving keywords to database repository

---
+++ src/database/UserPromptrepository.py
@@ -0,0 +1,28 @@
+import sqlalchemy as sa
+from sqlalchemy import create_engine, Table, MetaData
+from src.database.db_connection import DBConnection
+import logging
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+
+
+
+class UserPromptrepository:
+    def __init__(self):
+        self.db_connection = DBConnection()
+        self.engine = self.db_connection.get_engine()
+        self.metadata = MetaData()
+    
+    def saving_keywords(self,comment,user,keywords):
+        usaer_prompt = Table('usaer_prompt', self.metadata, autoload_with=self.engine)
+
+        with self.engine.connect() as conn:
+            with conn.begin():
+                stmt = sa.insert(usaer_prompt).values(
+                    prompt_text=comment,
+                    prompt_keyword=keywords,
+                    user_name=user
+                    )
+                conn.execute(stmt)
+                logging.debug("keywords are saved successfully!")
+
+    
